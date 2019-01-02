f0f181cd8f15cfc5322bdc3d65b36180394ef3ba
allstar456 <191050832@qq.com>
2019-01-02 15:12:11 +0800

    初始化

---
+++ CZYLib1.framework/Versions/A/Headers/GGLog.h
@@ -0,0 +1,16 @@
+//
+//  GGLog.h
+//  CZYLib1
+//
+//  Created by 陈志勇 on 2018/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GGLog : NSObject
+- (void)printMsg:(NSString *)msg;
+@end
+
+NS_ASSUME_NONNULL_END
